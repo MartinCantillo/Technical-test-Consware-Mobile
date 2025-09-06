2785456bd549f96604cfa5ca9714b973e1b42e98
Martin9 <martinjcantillo9@gmail.com>
2025-09-06 12:20:11 -0500

    feat: add data

---
+++ lib/data/TaskData.dart
@@ -0,0 +1,14 @@
+import 'package:consware/model/task.dart';
+
+final List<Task> tasks = [
+  Task(id: 1, title: 'Comprar pan', isCompleted: false),
+  Task(id: 2, title: 'Estudiar Flutter', isCompleted: true),
+  Task(id: 3, title: 'Hacer ejercicio', isCompleted: false),
+  Task(id: 4, title: 'Leer un libro', isCompleted: false),
+  Task(id: 5, title: 'Enviar correo', isCompleted: true),
+  Task(id: 6, title: 'Llamar a mamá', isCompleted: false),
+  Task(id: 7, title: 'Cocinar cena', isCompleted: true),
+  Task(id: 8, title: 'Limpiar habitación', isCompleted: false),
+  Task(id: 9, title: 'Ir al supermercado', isCompleted: false),
+  Task(id: 10, title: 'Revisar proyecto', isCompleted: true),
+];
